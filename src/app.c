717f4e29d16209b881535516028ad153c31be5be
Harley Swick <fancycade@mycanofbeans.com>
2022-10-26 00:22:56 +0000

    Implement uuid with openssl

--- src/app.c
+++ src/app.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 #include <sqlite3.h>
-#include <uuid/uuid.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <pwd.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <openssl/rand.h>
 #include "app.h"
+
+// Taken from: https://gist.github.com/kvelakur/9069c9896577c3040030
+static int
+uuid_v4_gen(char *buffer)
+{
+	union
+	{
+		struct
+		{
+			uint32_t time_low;
+			uint16_t time_mid;
+			uint16_t time_hi_and_version;
+			uint8_t  clk_seq_hi_res;
+			uint8_t  clk_seq_low;
+			uint8_t  node[6];
+		};
+		uint8_t __rnd[16];
+	} uuid;
+
+	int rc = RAND_bytes(uuid.__rnd, sizeof(uuid));
+
+	// Refer Section 4.2 of RFC-4122
+	// https://tools.ietf.org/html/rfc4122#section-4.2
+	uuid.clk_seq_hi_res = (uint8_t) ((uuid.clk_seq_hi_res & 0x3F) | 0x80);
+	uuid.time_hi_and_version = (uint16_t) ((uuid.time_hi_and_version & 0x0FFF) | 0x4000);
+
+	snprintf(buffer, 38, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+			uuid.time_low, uuid.time_mid, uuid.time_hi_and_version,
+			uuid.clk_seq_hi_res, uuid.clk_seq_low,
+			uuid.node[0], uuid.node[1], uuid.node[2],
+			uuid.node[3], uuid.node[4], uuid.node[5]);
+
+	return rc;
+}
 
 void
 help(void)
@@ -144,10 +178,8 @@ create_tables(sqlite3 *db)
 int
 new(sqlite3 *db)
 {
-	char uuid[37];
-	uuid_t binuuid;
-	uuid_generate_random(binuuid);
-	uuid_unparse_lower(binuuid, uuid);
+	char uuid[38];
+	uuid_v4_gen(uuid);
 
 	char zdir[200];
         char* homedir = getenv("HOME");
@@ -527,11 +559,8 @@ slurp(sqlite3 *db, const char *path)
 			goto end;
 		}
 
-		char uuid[37];
-		uuid_t binuuid;
-		uuid_generate_random(binuuid);
-		uuid_unparse_lower(binuuid, uuid);
-
+		char uuid[38];
+		uuid_v4_gen(uuid);
 
 		sqlite3_bind_text(stmt, 1, uuid, strlen(uuid), SQLITE_STATIC);
 		sqlite3_bind_text(stmt, 2, buffer, strlen(buffer), SQLITE_TRANSIENT);
