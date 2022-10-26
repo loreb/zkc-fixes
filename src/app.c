26daeeea73d78d21c78b0ec0eccab052699708bd
Harley Swick <fancycade@mycanofbeans.com>
2022-10-26 00:26:03 +0000

    Implement sha256 with openssl

--- src/app.c
+++ src/app.c
@@ -7,7 +7,25 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <openssl/rand.h>
+#include <openssl/sha.h>
 #include "app.h"
+
+// Taken from https://stackoverflow.com/questions/2262386/generate-sha256-with-openssl-and-c
+static void
+sha256_string(char *string, char outputBuffer[65])
+{
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, string, strlen(string));
+    SHA256_Final(hash, &sha256);
+    int i = 0;
+    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
+    {
+        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
+    }
+    outputBuffer[64] = 0;
+}
 
 // Taken from: https://gist.github.com/kvelakur/9069c9896577c3040030
 static int
